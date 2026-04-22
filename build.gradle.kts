bcba34a45b809c9f295bfe620a3e162eaf62165d
ShaneBeee <shanebolenback@me.com>
2026-04-21 22:32:04 -0700

    prepare for release of 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ configurations.matching { it.isCanBeResolved }.configureEach {
 }
 
 // Version of SkriptRegistration
-val projectVersion = "1.0.4"
+val projectVersion = "1.0.5"
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 
